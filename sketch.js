e9a78f864ca17459b56a86bce926d8651adc852f
saekkkj <100127930+saekkkj@users.noreply.github.com>
2022-03-29 11:01:35 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,11 +2,13 @@ var garden,rabbit;
 var gardenImg,rabbitImg;
 var apple, appleImg
 var leaf, leafImg
+var Orange,orangeimg
 function preload(){
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
   appleImg = loadImage("apple.png")
   leafImg = loadImage("leaf.png")
+  orangeimg= loadImage("orangeleaf.png")
 }
 
 function setup(){
@@ -31,20 +33,12 @@ function draw() {
   rabbit.collide(edges);
   rabbit.x=World.mouseX
   var select_sprites = Math.round(random(1,3));
-  if (frameCount % 80 == 0) {
-  if (select_sprites  == 1) 
-  { createApples(); }
-  else if  (select_sprites == 2) {         createApples(); }
-else {  createApples(); } }
-  drawSprites();
-  
-  var select_sprites = Math.round(random(1,3));
   if (frameCount % 100 == 0) {
   if (select_sprites  == 1) 
   { createLeaves(); }
-  else if  (select_sprites == 5) {
-    createLeaves(); }
-else {  createLeaves(); } }
+  else if  (select_sprites == 2) {
+    createApples(); }
+else {  createOrange(); } }
   drawSprites();
 }
 function createApples() {
@@ -60,4 +54,11 @@ leaf.addImage(leafImg);
 leaf.scale=0.1;
 leaf.velocityY=5;
 leaf.lifetime=150;
+}
+function createOrange(){
+  Orange=createSprite(random(50,350),40,10,10)
+  Orange.addImage(orangeimg);
+  Orange.scale=0.1;
+  Orange.velocityY=5;
+  Orange.lifetime=150; 
 }
